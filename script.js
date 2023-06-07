f3b413abbb8695909b9d28ba325b2c923f30691a
Jay <careerforjay@gmail.com>
2023-06-07 12:42:47 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,33 @@
+// countless arrage methods in javascript
+// push is a method, a method is technically a function
+// push - adds to the back of array
+const friends = ["mike", "steve", "pete"];
+const newLength = friends.push("Jay");
+console.log(friends);
+console.log(newLength);
+//shift - adds to the front of an array
+friends.unshift("john");
+console.log(friends);
+//pop - removes last element of an array
+const popped = friends.pop();
+console.log(popped);
+friends.pop();
+console.log(friends);
+//shift - removes 1st element of an array
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf("steve"));
+console.log(friends.indexOf("bob"));
+friends.push(23);
+console.log(friends.includes("steve"));
+console.log(friends.includes("bob"));
+console.log(friends.includes(23));
+console.log(friends.includes("23"));
+//most used case for includes.
+if (friends.includes("mike")) console.log("you have a friends called mike");
+else console.log("nope");
+if (friends.includes("Mike")) console.log("you have a friends called mike");
+else console.log("nope");
+if (friends.includes("ike")) console.log("you have a friends called mike");
+else console.log("nope");
